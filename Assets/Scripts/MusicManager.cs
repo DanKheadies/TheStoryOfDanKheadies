91dd98d033f9156413924ee87e603a5d93d4fe46
DESKTOP-L4FMVUO\DacoTacoFlame <davidwcorso@gmail.com>
2017-07-28 11:15:40 -0400

    DK017 Tiled Sprites and Music Manager

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -39,7 +39,7 @@ public class MusicManager : MonoBehaviour
                 musicTracks[currentTrack].Play();
             }
         }
-        else
+        else if (!bMusicExists)
         {
             musicTracks[currentTrack].Stop();
         }
